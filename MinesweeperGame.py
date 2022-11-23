285b78a7fcc6fc0b29a6bad1def723fb85524e29
Fedrice <111098179+Fedrice@users.noreply.github.com>
2022-11-22 19:39:31 -0500

    Update MinesweeperGame.py

--- MinesweeperGame.py
+++ MinesweeperGame.py
@@ -1,7 +1,41 @@
 import tkinter as tk
+from tkinter import Frame
+import utils
+import Settings
 root=tk.Tk()
+#general settings of the window
+root.configure(bg='red')
+root.geometry(f'{Settings.width}x{Settings.height}')
+root.title("Minesweeper")
+root.resizable(False,False)
 
-root.geometry("300x300")
-root.title("Minesweeper Fedrice Style")
+top_frame= Frame(
+    root,
+    bg='red', 
+    width=Settings.width,
+    height=utils.height_percentage(25)
+)
 
+left_frame= Frame(
+    root,
+    bg='red'
+    width=utils.width_percentage(25),
+    height=utils.height_percentage(75)
+)
+top_frame.place(x=0,y=0)
+left_frame.place(x=0,y=utils.height_percentage(25))
+center_frame= Frame (
+    root,
+    bg='red' , 
+    width=utils.width_percentage(75),
+    height=utils.height_percentage(75)
+)
+center_frame.place(
+    x=utils.width_percentage(25),
+    y=utils.height_percentage(25),
+)
+
+
+
+#run the window
 root.mainloop()
