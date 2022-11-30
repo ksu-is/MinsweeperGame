0c9b0854554baf2c5286b352f1fc2458f9be2802
Fedrice <111098179+Fedrice@users.noreply.github.com>
2022-11-29 21:13:05 -0500

    Update MinesweeperGame.py

--- MinesweeperGame.py
+++ MinesweeperGame.py
@@ -1,41 +1,53 @@
-import tkinter as tk
-from tkinter import Frame
+
+from tkinter import *
 import utils
 import Settings
-root=tk.Tk()
+from Cell import cell
+root=Tk()
 #general settings of the window
-root.configure(bg='red')
+root.configure(bg='black')
 root.geometry(f'{Settings.width}x{Settings.height}')
 root.title("Minesweeper")
 root.resizable(False,False)
 
 top_frame= Frame(
     root,
-    bg='red', 
+    bg='black', 
+    width=Settings.width,
+    height=utils.height_percentage(10),
+    
+    )
+
+game_area= Frame (
+    root,
+    bg='black' , 
     width=Settings.width,
     height=utils.height_percentage(25)
-)
+    )
 
-left_frame= Frame(
-    root,
-    bg='red'
-    width=utils.width_percentage(25),
-    height=utils.height_percentage(75)
-)
 top_frame.place(x=0,y=0)
-left_frame.place(x=0,y=utils.height_percentage(25))
-center_frame= Frame (
-    root,
-    bg='red' , 
-    width=utils.width_percentage(75),
-    height=utils.height_percentage(75)
-)
-center_frame.place(
-    x=utils.width_percentage(25),
-    y=utils.height_percentage(25),
+game_area.place(
+    x=0,
+    y=utils.height_percentage(10),
 )
 
 
 
-#run the window
+
+
+for x in range(Settings.grid_size):
+    for y in range(Settings.grid_size):
+        c=cell(x,y)
+        c.create_btn_object(game_area)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
+cell.create_cell_count_label(top_frame)
+cell.cell_count_label_object.place(
+    x=0,y=0
+)
+cell.mines_1() 
+
+
+
 root.mainloop()
